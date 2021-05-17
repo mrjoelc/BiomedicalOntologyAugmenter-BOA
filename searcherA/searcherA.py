7e0a385bdd1f6ff3ee0fbc5d4f75fc2c7afe6b01
Gioele <gcageggi@gmail.com>
2021-05-17 09:07:49 +0200

    add kafka + searcherA

---
+++ searcherA/searcherA.py
@@ -0,0 +1,16 @@
+from kafka import KafkaConsumer
+from json import loads
+
+consumer = KafkaConsumer(
+    'scholar',
+    bootstrap_servers=['localhost:9093'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='my-group-id',
+    value_deserializer=lambda x: loads(x.decode('utf-8'))
+)
+
+for event in consumer:
+    event_data = event.value
+    # Do whatever you want
+    print(event_data)
